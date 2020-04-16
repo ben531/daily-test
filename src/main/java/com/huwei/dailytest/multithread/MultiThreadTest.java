96926afb0150a2e0de17a2c9df467214dcba4667
肥宅哥哥 <a@gmail.com>
2020-04-16 20:35:50 +0800

    multithread test

---
+++ src/main/java/com/huwei/dailytest/multithread/MultiThreadTest.java
@@ -0,0 +1,21 @@
+package com.huwei.dailytest.multithread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MultiThreadTest {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 20; i++) {
+            executorService.execute(new myThread());
+        }
+        executorService.shutdown();
+    }
+}
+
+class myThread implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("myThread is run" + Thread.currentThread().getName());
+    }
+}
