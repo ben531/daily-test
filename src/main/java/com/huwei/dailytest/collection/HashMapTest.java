c8f6f06cfe3cbc0ba21d047b759d8927617db989
肥宅哥哥 <a@gmail.com>
2020-04-16 19:45:57 +0800

    hashmap test

---
+++ src/main/java/com/huwei/dailytest/collection/HashMapTest.java
@@ -0,0 +1,33 @@
+package com.huwei.dailytest.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<String, String>() {
+            {
+                put("姓名", "张三");
+                put("性别", "男");
+                put("年龄", "22");
+            }
+        };
+
+        hashMap.forEach((k, v) -> {
+            System.out.println(k + ":" + v);
+        });
+
+        for (Map.Entry<String, String> entry :
+                hashMap.entrySet()) {
+            System.out.println(entry.getKey() + "->" + entry.getValue());
+        }
+
+        for (String key:
+             hashMap.keySet()) {
+            System.out.println(key+" >>> " +hashMap.get(key));
+        }
+
+
+    }
+}
