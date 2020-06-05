b6cfc407b792bf1b2867424a53de8ab34db8520e
wxhuxd <huxdc@yutong.com>
2020-06-05 09:31:43 +0800

    增加arrayList

---
+++ src/main/java/com/huwei/dailytest/collection/ArrayListTest.java
@@ -0,0 +1,15 @@
+package com.huwei.dailytest.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>() {
+            {
+
+            }
+        };
+    }
+
+}
