7255149a82b65cd98b8a52673b024d6f50655c94
wxhuxd <huxdc@yutong.com>
2020-06-23 14:16:33 +0800

    list->map

--- src/main/java/com/huwei/dailytest/collection/HashMapTest.java
+++ src/main/java/com/huwei/dailytest/collection/HashMapTest.java
@@ -1,43 +1,60 @@
 package com.huwei.dailytest.collection;
 
+import com.huwei.dailytest.entity.User;
+
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class HashMapTest {
     public static void main(String[] args) {
 
-        Map<Integer, List<String>> hashMap = new HashMap<Integer, List<String>>() {
-
+        List<User> list = new ArrayList<User>(){
             {
-                put(1, new ArrayList<String>() {
-                    {
-                        add("a");
-                    }
-                });
-                put(2, new ArrayList<String>() {
-                    {
-                        add("c");
-                        add("d");
-                    }
-                });
+                add(new User("zz",11));
+//                add(new User("zz",19));
+                add(new User("ls",23));
+                add(new User("ww",15));
             }
         };
-        boolean empty = hashMap.isEmpty();
-
-        // 操作iterater时,可以remove某个对象
-        for (Map.Entry<Integer, List<String>> entry : hashMap.entrySet()) {
-            List<String> value = entry.getValue();
-            Iterator<String> iterator = value.iterator();
-            while (iterator.hasNext()) {
-                String next = iterator.next();
-                if (next.equals("a")) {
-                    iterator.remove();
-                }
-            }
-        }
-        hashMap.forEach((k, v) -> {
-            System.out.println(k + "->" + v);
+        // u.getName必须唯一
+        Map<String, User> collect = list.stream().collect(Collectors.toMap(u -> u.getName(), Function.identity()));
+        collect.forEach((k,v)->{
+            System.out.println(k+"->"+v);
         });
 
+
+//        Map<Integer, List<String>> hashMap = new HashMap<Integer, List<String>>() {
+//            {
+//                put(1, new ArrayList<String>() {
+//                    {
+//                        add("a");
+//                    }
+//                });
+//                put(2, new ArrayList<String>() {
+//                    {
+//                        add("c");
+//                        add("d");
+//                    }
+//                });
+//            }
+//        };
+//        boolean empty = hashMap.isEmpty();
+//
+//        // 操作iterater时,可以remove某个对象
+//        for (Map.Entry<Integer, List<String>> entry : hashMap.entrySet()) {
+//            List<String> value = entry.getValue();
+//            Iterator<String> iterator = value.iterator();
+//            while (iterator.hasNext()) {
+//                String next = iterator.next();
+//                if (next.equals("a")) {
+//                    iterator.remove();
+//                }
+//            }
+//        }
+//        hashMap.forEach((k, v) -> {
+//            System.out.println(k + "->" + v);
+//        });
 
 //        HashMap<String, String> hashMap = new HashMap<String, String>() {
 //            {
--- src/main/java/com/huwei/dailytest/fanxing/FanxingTest.java
+++ src/main/java/com/huwei/dailytest/fanxing/FanxingTest.java
@@ -19,27 +19,35 @@ public class FanxingTest {
 
     protected <T> GetSet<T> getSet(String hashKey) {
         return GetSetBuilder.of(() -> {
-                return (T) setOrders.get(hashKey);
-        },(value) -> {
-            System.out.println(value);
+            System.out.println("执行get方法");
+            setOrders.put(hashKey, "value");
+            return (T) setOrders.get(hashKey);
+        }, (value) -> {
+            System.out.println("执行set方法");
             setOrders.put(hashKey, value);
+//            setOrders.put(hashKey, "value");
+
         });
     }
 
     public static void main(String[] args) {
-        FanxingTest fanxingTest=new FanxingTest("1");
+        FanxingTest fanxingTest = new FanxingTest("1");
         GetSet<String> test = fanxingTest.test;
-        fanxingTest.setOrders.forEach((k,v)->{
-            System.out.println("k:"+"->"+v);
+        fanxingTest.setOrders.forEach((k, v) -> {
+            System.out.println("k:" + "->" + v);
         });
+        String s2 = test.get();
+        System.out.println("s2: "+s2);
         test.set("string-1");
-        fanxingTest.setOrders.forEach((k,v)->{
-            System.out.println("k:"+"->"+v);
+        String s1 = test.get();
+        System.out.println("s1:"+s1);
+        fanxingTest.setOrders.forEach((k, v) -> {
+            System.out.println("k:" + "->" + v);
         });
 
-        FanxingTest fanxingTest1=new FanxingTest("1");
+        FanxingTest fanxingTest1 = new FanxingTest("1");
         String s = fanxingTest1.test.get();
-        System.out.println("fanxingtest1: "+s);
+        System.out.println("fanxingtest1: " + s);
 
     }
 
