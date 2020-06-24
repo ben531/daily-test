2085d7e5bb3b940fa7a2f8859bf72a4ad256b98f
wxhuxd <huxdc@yutong.com>
2020-06-24 09:24:18 +0800

    split

---
+++ src/main/java/com/huwei/dailytest/collection/StringTest.java
@@ -0,0 +1,28 @@
+package com.huwei.dailytest.collection;
+
+import cn.hutool.core.util.StrUtil;
+
+import java.util.Arrays;
+
+/**
+ * jdk的split方法与hutool的split方法不同
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        String str = "a,,b,c";
+        String[] split = str.split(",");
+        System.out.println(split.length);
+        System.out.println(Arrays.asList(split));
+
+        str = "a,b,c,,";
+        String[] split1 = str.split(",");
+        System.out.println(split1.length);
+        System.out.println(Arrays.asList(split1));
+
+        String[] split2 = StrUtil.split(str, ",");
+        System.out.println(split2.length);
+        System.out.println(Arrays.asList(split2));
+
+
+    }
+}
